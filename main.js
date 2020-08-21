9893934488aeb5c593714f3f40b2a5a3fadda618
j3ffh95 <j3ffh95@gmail.com>
2020-08-20 23:30:07 -0400

    Store the x and y location

--- main.js
+++ main.js
@@ -5,3 +5,7 @@ let zoomActivated = false;
 window.addEventListener("keydown", (e) => {
   if (e.key === "z") zoomActivated = !zoomActivated;
 });
+
+window.addEventListener("mousemove", (e) => {
+  const { clientX: x, clientY: y } = e;
+});
